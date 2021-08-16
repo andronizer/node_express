d9e81cd2998979cf5ac8ac0c63c5ca70fcddef1f
andronizer <olloloshik@mail.ru>
2021-08-16 16:58:08 +0300

    migration added

---
+++ migrations/migration_table.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('comments', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      tag: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('tags')
+  }
+};
