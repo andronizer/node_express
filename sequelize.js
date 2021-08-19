c142cfefd326ef8eeb70f1cd694d2e2db6f91e98
andronizer <olloloshik@mail.ru>
2021-08-19 13:07:10 +0300

    middleware auth add

--- sequelize.js
+++ sequelize.js
@@ -1,13 +1,13 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
   development: {
-    dialect: "postgres",
+    dialect: 'postgres',
     username: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
-    host: process.env.HOST,    
-    port: process.env.PORT
+    host: process.env.HOST,
+    port: process.env.PORT,
   },
 };
 
